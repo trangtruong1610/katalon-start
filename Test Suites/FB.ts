00c453d3b3b8ef1b1e5fcd137499390502058d1b
Trang Truong <trang.truong@gigacover.com>
2020-06-20 14:03:57 +0700

    demo like fanpage on facebook

---
+++ Test Suites/FB.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FB</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>526fa1c9-0954-4900-8671-c8201febbba8</testSuiteGuid>
+   <testCaseLink>
+      <guid>557b8b6d-deb9-492c-82e7-548dc124d347</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Facebook</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>a9a20c93-ca58-47d7-a7a7-a2f5b176364a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/account</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>a9a20c93-ca58-47d7-a7a7-a2f5b176364a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>184b819f-4e16-4310-a5fc-6c6b80c1d7f2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>a9a20c93-ca58-47d7-a7a7-a2f5b176364a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>ad8c97f9-e333-4d3a-9c34-54e412ac5495</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
